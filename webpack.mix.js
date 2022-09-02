8a5edbb2442c8f22de4e6c1144c9fd8b08133afc
SalyOne <salome.batkhadze@gmail.com>
2022-09-02 20:15:12 +0400

    add fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,5 @@ let mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'assets/dist')
     .sass('src/scss/app.scss', 'assets/dist')
+    // .sass('src/scss/fonts.scss', 'assets/dist')
+// add /PCfy/assets/ to coverted font file
